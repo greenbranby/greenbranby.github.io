08477ce15ec84d11656d0f77f9e08c07eaeafe7a
Sandy Green <green7786@comcast.net>
2018-06-23 15:24:43 -0600

    added animations with css and jquery

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Portfolio Site
+My Portfolio Site
+
+Currently filled with projects from the Treehouse.com Techdegree program with projects that showcase my progression with HTML5, CSS and JavaScript, as well as SASS, Git, AJAX, DevTools and React.
--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,6 @@
 $(document).ready(function () {
 
 
-$(".spin").on("mouseenter mouseleave touchstart", function(e){
-    if(e.type == 'touchstart') {
-      $(this).off('mouseenter mouseleave');
-    }
-
-    $(this).toggleClass("slide");
-});
 
 $("a.social").on("mouseenter mouseleave touchstart", function(e){
     if(e.type == 'touchstart') {
@@ -33,6 +26,35 @@ $("a.button").on("mouseenter mouseleave touchstart", function(e){
         link.removeEventListener("mouseleave", toggleImage);
       }
 
-      link.classList.toggle("slide");
+      link.classList.toggle("hover");
     }
+// Scroll Animations
+let $animation_elements = $('.animation-element');
+let $window = $(window);
+
+//an event handler that listens for the scroll event
+$window.on('scroll', check_if_in_view);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+
 }); //end ready
