9d0c3c62e84e185d18e1f9d0ecd7292d82e2dac6
Sandy Green <green7786@comcast.net>
2018-06-22 18:56:01 -0600

    more jquery tweaks

--- js/jquery.js
+++ js/jquery.js
@@ -9,6 +9,24 @@ $(".spin").on("mouseenter mouseleave touchstart", function(e){
     $(this).toggleClass("slide");
 });
 
+$("a.social").on("mouseenter mouseleave touchstart", function(e){
+    if(e.type == 'touchstart') {
+      $(this).off('mouseenter mouseleave');
+    }
+
+    $(this).toggleClass("hover");
+});
+
+
+$("a.button").on("mouseenter mouseleave touchstart", function(e){
+    if(e.type == 'touchstart') {
+      $(this).off('mouseenter mouseleave');
+    }
+
+    $(this).toggleClass("hover");
+});
+
+
     function toggleImage(e) {
       if (e.type == "touchstart") {
         link.removeEventListener("mouseenter", toggleImage);
